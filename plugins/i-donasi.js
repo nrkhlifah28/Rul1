7eb9a9b50ffd42ee544a08301fcbf392039a6468
nrkhlifah28 <94022360+nrkhlifah28@users.noreply.github.com>
2022-09-15 00:03:30 +0700

    Update i-donasi.js

--- plugins/i-donasi.js
+++ plugins/i-donasi.js
@@ -8,13 +8,13 @@ let handler = async (m, { conn, usedPrefix }) => {
       extendedTextMessage: {
       text: `
 ╭─「 Donasi • Dana 」
-│ • im3 [085822347348]
-│ • axis  [083843192208]
-│ • Dana  [085822347348]
-│ • Saweria  [https://saweria.co/arullofc]
+│ • Three(3) [089618728196]
+│ • XL [087892870587]
+│ • Dana  [089618728196]
+│ • Saweria  [https://saweria.co/nrkhlifah28]
 ╰────
 ╭─「 *NOTE* 」
-│ > Ingin donasi? Wa.me/6285822347348
+│ > Ingin donasi? Wa.me/6289618728196
 │ _Hasil donasi akan digunakan buat sewa_
 │ _atau beli *RDP/VPS* agar bot bisa jalan_
 │ _24jam tanpa kendala_
